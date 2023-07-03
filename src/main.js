dd412d2abb6243a80d1185ede10bc26aa1f9b7dc
nikrassel <nikrassel@mail.ru>
2023-07-03 17:13:21 +0300

    add routing and pages templates

--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import 'materialize-css'
+import 'materialize-css/dist/js/materialize.min'
 
 createApp(App).use(store).use(router).mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -8,12 +8,58 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/login',
+    name: 'login',
+    meta: {layout: 'empty'},
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    meta: {layout: 'empty'},
+    component: () => import('../views/Register.vue')
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: {layout: 'main'},
+    component: () => import('../views/Categories.vue')
+  },
+  {
+    path: '/detail',
+    name: 'detail',
+    meta: {layout: 'main'},
+    component: () => import('../views/DetailRecord.vue')
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: {layout: 'main'},
+    component: () => import('../views/History.vue')
+  },
+  {
+    path: '/home',
+    name: 'homepage',
+    meta: {layout: 'main'},
+    component: () => import('../views/Home.vue')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: {layout: 'main'},
+    component: () => import('../views/Planning.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: {layout: 'main'},
+    component: () => import('../views/Profile.vue')
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: {layout: 'main'},
+    component: () => import('../views/Record.vue')
   }
 ]
 
