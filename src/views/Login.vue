45f8b9939cbc0619f2dfd7d3ff811240bf78a49d
nikrassel <nikrassel@mail.ru>
2023-07-04 17:38:19 +0300

    add login form validation

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,24 +1,40 @@
 <template>
-    <form class="card auth-card">
+    <form class="card auth-card" @submit.prevent="submitHandler">
   <div class="card-content">
     <span class="card-title">Домашняя бухгалтерия</span>
     <div class="input-field">
       <input
           id="email"
           type="text"
-          class="validate"
+          v-model.trim="email"
+          :class="{invalid: (v$.email.$dirty && !v$.email.required.$response) || (v$.email.$dirty && !v$.email.email.$response)}"
       >
       <label for="email">Email</label>
-      <small class="helper-text invalid">Email</small>
+      <small 
+        class="helper-text invalid"
+        v-if="v$.email.$dirty && !v$.email.required.$response"
+      >Поле Email не должно быть пустым</small>
+      <small 
+        class="helper-text invalid"
+        v-if="v$.email.$dirty && !v$.email.email.$response"
+      >Email введен неккоректно</small>
     </div>
     <div class="input-field">
       <input
           id="password"
           type="password"
-          class="validate"
+          v-model.trim="password"
+          :class="{invalid: (v$.password.$dirty && !v$.password.required.$response) || (v$.password.$dirty && !v$.password.minLength.$response)}"
       >
       <label for="password">Пароль</label>
-      <small class="helper-text invalid">Password</small>
+      <small 
+        class="helper-text invalid"
+        v-if="v$.password.$dirty && !v$.password.required.$response"
+      >Поле Password не должно быть пустым</small>
+      <small 
+        class="helper-text invalid"
+        v-if="v$.password.$dirty && !v$.password.minLength.$response"
+      >Password должен содержать как минимум {{ v$.password.minLength.$params.min }} символов</small>
     </div>
   </div>
   <div class="card-action">
@@ -34,8 +50,39 @@
 
     <p class="center">
       Нет аккаунта?
-      <a href="/">Зарегистрироваться</a>
+      <router-link to="/register">Зарегистрироваться</router-link>
     </p>
   </div>
 </form>
 </template>
+
+<script>
+import { useVuelidate } from '@vuelidate/core'
+import { required, email, minLength } from '@vuelidate/validators'
+export default {
+  // setup: () => ({  }),
+  name: 'login',
+  data: () => ({
+    v$: useVuelidate(),
+    email: '',
+    password: ''
+  }),
+  methods: {
+    submitHandler() {
+      this.v$.$validate()
+      if(!this.v$.$error) {
+        const formData = {
+          email: this.email,
+          password: this.password
+        }
+        console.log(formData)
+        this.$router.push('/')
+      }
+    }
+  },
+  validations: {
+    email: {email, required},
+    password: {required, minLength: minLength(6)}
+  }
+}
+</script>
